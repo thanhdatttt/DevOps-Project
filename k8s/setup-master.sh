6d993ffa72def9f4d6e0d5b989cf27b4cb49a2d0
Simpolette <giangminhminh13@gmail.com>
2026-05-14 14:22:16 +0700

    chore: clean setup master

--- k8s/setup-master.sh
+++ k8s/setup-master.sh
@@ -20,6 +20,9 @@ kubectl port-forward service/argocd-server -n argocd 8080:443
 # enable ingress in the values file `server.ingress.enabled` and either
 
 # CoreDNS must be run before this, if not restart it first
+kubectl rollout restart deployment coredns -n kube-system
+kubectl rollout status deployment coredns -n kube-system
+
 # ArgoCD observe the charts
 kubectl apply -f applicationset-microservices.yaml -n argocd
 kubectl apply -f appset-infra-external.yaml -n argocd
@@ -30,12 +33,6 @@ kubectl apply -f appset-obs-internal.yaml -n argocd
 # Turn on Ip in IP in Calico
 
 
-# Watch
-❯ kubectl port-forward service/argocd-server -n argocd 8080:443
-
-
-# helm dep update argo/
-
-# helm repo upgrade
-
-# helm install argo-cd argo-cd/
+#HELPFUL
+ kubectl rollout restart deployment argocd-applicationset-controller -n argocd
+ kubectl logs -n argocd -l app.kubernetes.io/name=argocd-applicationset-controller -f
